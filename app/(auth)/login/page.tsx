0a3d9e911034afebebf0cef73c497dddefca6156
Sanjula Herath <sanjula692@gmail.com>
2025-12-04 15:17:03 +0530

    feat: add login page with Supabase authentication and remove example environment file.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -103,7 +103,7 @@ export default function LoginPage() {
             </Button>
           </form>
           <div className="mt-6 text-center text-sm">
-            <span className="text-gray-600">Don't have an account? </span>
+            <span className="text-gray-600">Don&apos;t have an account? </span>
             <Link
               href="/signup"
               className="font-medium text-black dark:text-white hover:underline"
